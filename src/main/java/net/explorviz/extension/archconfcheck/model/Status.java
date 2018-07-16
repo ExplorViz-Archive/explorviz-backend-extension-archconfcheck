1aa3ed681c75decba6b723f8d9256b9ccb4e4658
timhackel@gmx.de <timhackel@gmx.de>
2018-07-16 11:29:53 +0200

    added an enum status, that is used for the datamodel wip: added the functionality of comparing, needs to be altered, so it also is capable of doing so with the ASMODELED candidates, everything else should be working fine

---
+++ src/main/java/net/explorviz/extension/archconfcheck/model/Status.java
@@ -0,0 +1,13 @@
+package net.explorviz.extension.archconfcheck.model;
+
+/**
+ * This flag is attached to each entity of the data model. It shows whether an
+ * entity is modified or not.
+ *
+ * @author 4llQuantor
+ *
+ */
+
+public enum Status {
+	ASMODELLED, WARNING, GHOST;
+}
--- src/main/java/net/explorviz/extension/archconfcheck/resources/ArchConCheckRessource.java
+++ src/main/java/net/explorviz/extension/archconfcheck/resources/ArchConCheckRessource.java
@@ -1,6 +1,7 @@
 package net.explorviz.extension.archconfcheck.resources;
 
 import java.io.File;
+import java.util.Random;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,8 +10,15 @@ import javax.ws.rs.Produces;
 
 import net.explorviz.api.ExtensionAPI;
 import net.explorviz.api.ExtensionAPIImpl;
+import net.explorviz.extension.archconfcheck.model.Status;
 import net.explorviz.extension.modeleditor.resources.ModelLandscapeResource;
+import net.explorviz.model.application.Application;
+import net.explorviz.model.application.Clazz;
+import net.explorviz.model.application.Component;
 import net.explorviz.model.landscape.Landscape;
+import net.explorviz.model.landscape.Node;
+import net.explorviz.model.landscape.NodeGroup;
+import net.explorviz.model.landscape.System;
 import net.explorviz.server.helper.FileSystemHelper;
 
 // @Secured
@@ -21,8 +29,8 @@ public class ArchConCheckRessource extends ModelLandscapeResource {
 
 	private final ExtensionAPIImpl api = ExtensionAPI.get();
 	// TODO build new fatjar!
-	// private static final String MODEL_REPOSITORY = super.getModelRepository()
-	private static final String MODEL_REPOSITORY = "modellRepository";
+	private final String MODEL_REPOSITORY = super.getModelRepository();
+	private static final String saveAs = "status";
 
 	@GET
 	// the timestamps parameter is conventionalized to be first: monitored timestamp
@@ -76,10 +84,118 @@ public class ArchConCheckRessource extends ModelLandscapeResource {
 
 		// TODO Do magic code that compares the two Landscapes and returns a new and
 		// better with attributes Landscape and such
-		final Landscape calculatedLandscape = null;
+		final Landscape calculatedLandscape = new Landscape();
+		calculatedLandscape.initializeID();
+		calculatedLandscape.setOverallCalls(new Random().nextInt(300000));
 
+		for (final net.explorviz.model.landscape.System child1 : monitoredLandscape.getSystems()) {
+			boolean compareCheck = false;
+			for (final net.explorviz.model.landscape.System child2 : modelLandscape.getSystems()) {
+				if (child1.getName().equals(child2.getName())) {
+					// there is a system in each landscape with the same name
+					final System comparedSystem = new System();
+					comparedSystem.initializeID();
+					comparedSystem.setName(child1.getName());
+					comparedSystem.setParent(calculatedLandscape);
+					comparedSystem.getExtensionAttributes().put(saveAs, Status.ASMODELLED);
+					calculatedLandscape.getSystems().add(comparedSystem);
+					compareCheck = true;
+					// now the following subcategories can be anything! oO
+					checkNodegroups(calculatedLandscape, child1, child2);
+					break;
+				}
+			}
+			if (compareCheck == false) {
+				// there is a system in the monitoredLandscape, that was not modeled!
+				final System warnSystem = new System();
+				warnSystem.initializeID();
+				warnSystem.setName(child1.getName());
+				warnSystem.setParent(calculatedLandscape);
+				warnSystem.getExtensionAttributes().put(saveAs, Status.WARNING);
+				// now every subcategory of this system is also a WARNING!
+				setStatusOfNodegroups(warnSystem, child1, Status.WARNING);
+				calculatedLandscape.getSystems().add(warnSystem);
+
+			}
+		}
+		// now the backwards search begins!
+		for (final net.explorviz.model.landscape.System child2 : modelLandscape.getSystems()) {
+			boolean compareCheck = false;
+			for (final net.explorviz.model.landscape.System child1 : monitoredLandscape.getSystems()) {
+				if (child1.getName().equals(child2.getName())) {
+					// just a reminder this was already covered in the forward search!
+					compareCheck = true;
+					break;
+				}
+			}
+			if (compareCheck == false) {
+				// this system was modeled but didn't show up in the monitoredLandscape
+				final System ghostSystem = new System();
+				ghostSystem.initializeID();
+				ghostSystem.setName(child2.getName());
+				ghostSystem.setParent(calculatedLandscape);
+				ghostSystem.getExtensionAttributes().put(saveAs, Status.GHOST);
+				// now every subcategory of this system is also a GHOST
+				setStatusOfNodegroups(ghostSystem, child2, Status.GHOST);
+				calculatedLandscape.getSystems().add(ghostSystem);
+
+			}
+		}
 		return calculatedLandscape;
-
 	}
 
+	private void setStatusOfNodegroups(final System comparedSystem, final System system, final Status status) {
+		for (final NodeGroup nodeGroup : system.getNodeGroups()) {
+			final NodeGroup comparedNG = nodeGroup;
+			comparedNG.getExtensionAttributes().put(saveAs, status);
+			setStatusOfNodes(comparedNG, nodeGroup, status);
+			comparedSystem.getNodeGroups().add(comparedNG);
+		}
+	}
+
+	private void setStatusOfNodes(final NodeGroup comparedNodeGroup, final NodeGroup nodeGroup, final Status status) {
+		for (final Node node : nodeGroup.getNodes()) {
+			final Node comparedNode = node;
+			comparedNode.getExtensionAttributes().put(saveAs, status);
+			setStatusOfApplications(comparedNode, node, status);
+			comparedNodeGroup.getNodes().add(comparedNode);
+		}
+	}
+
+	private void setStatusOfApplications(final Node comparedNode, final Node node, final Status status) {
+		for (final Application app : node.getApplications()) {
+			final Application comparedApp = app;
+			comparedApp.getExtensionAttributes().put(saveAs, status);
+			setStatusOfChildComponents(comparedApp, app, status);
+			comparedNode.getApplications().add(comparedApp);
+		}
+	}
+
+	private void setStatusOfChildComponents(final Application comparedApp, final Application app, final Status status) {
+		for (final Component component : app.getComponents()) {
+			final Component comparedComponent = component;
+			comparedComponent.getExtensionAttributes().put(saveAs, status);
+			setStatusOfComponents(comparedComponent, component, status);
+			app.getComponents().add(comparedComponent);
+		}
+	}
+
+	private void setStatusOfComponents(final Component comparedComponent, final Component component,
+			final Status status) {
+		for (final Component child : component.getChildren()) {
+			final Component comparedChild = child;
+			comparedChild.getExtensionAttributes().put(saveAs, status);
+			setStatusOfComponents(comparedChild, child, status);
+			comparedComponent.getChildren().add(comparedChild);
+		}
+		for (final Clazz clazz : component.getClazzes()) {
+			final Clazz comparedClazz = clazz;
+			comparedClazz.getExtensionAttributes().put(saveAs, status);
+			comparedComponent.getClazzes().add(comparedClazz);
+		}
+	}
+
+	private void checkNodegroups(final Landscape landscape, final System child1, final System child2) {
+
+	}
 }
